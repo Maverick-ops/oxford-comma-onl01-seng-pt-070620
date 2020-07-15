cf669839917be98818e15a43f7cfe90044251d09
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-15 00:41:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,8 @@ def oxford_comma(array)
      array.join(" and ")
      when 3
        array[0..2].join(" ,  ") << " , and #{array [2]}" 
+     else 
+       array[0...-1].join(" , ") << " , and #{array [-1]}"
        
       
 end
